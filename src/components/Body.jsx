ecf77dae260c0d6e7658ab96f4d39281ea650fca
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-24 02:55:35 +0600

    feat(routing): implements routing at body component successfully

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Login from './Login'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Browse from './Browse'
 
 const Body = () => {
+    const appRouter = createBrowserRouter([
+        {
+            path: "/",
+            element: <Login></Login>
+        },
+        {
+            path: "/browse",
+            element: <Browse></Browse>
+        }
+    ])
     return (
         <div>
-            <Login></Login>
+            <RouterProvider router={appRouter}></RouterProvider>
         </div>
     )
 }
