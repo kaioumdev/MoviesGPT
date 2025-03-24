d48778f218707b97cfe91318fa62dbb16359498e
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-24 16:00:12 +0600

    feat(styles): styles the header component & project logo

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from './Header'
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <div>
+            <Header></Header>
+            <img src="https://assets.nflxext.com/ffe/siteui/vlv3/f6e7f6df-6973-46ef-b98f-12560d2b3c69/web_tall_panel/BD-en-20250317-TRIFECTA-perspective_65450c69-6faa-4e0b-885c-8050f6db248f_large.jpg" alt="" />
+        </div>
     )
 }
 
