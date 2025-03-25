a3a7d75d9cdb94bfbf9bfe9a8b598e9aed4e7b05
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-26 00:58:23 +0600

    feat(trailer): got the trailer key & successfully show the videos

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -7,7 +7,7 @@ const VideoBackground = ({ movieId }) => {
         const json = await data.json()
         console.log(json)
         const filterData = json.results.filter((video) => video.type === 'Trailer')
-        const trailer = filterData[0]
+        const trailer = filterData.length ? filterData[0] : json.results[0]
         console.log(trailer)
     }
 
@@ -15,7 +15,9 @@ const VideoBackground = ({ movieId }) => {
         getMovieVideo()
     }, [])
     return (
-        <div>VideoBackground</div>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/5PSzFLV-EyQ?si=y8S-wnFRS0p9od_X" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
     )
 }
 
