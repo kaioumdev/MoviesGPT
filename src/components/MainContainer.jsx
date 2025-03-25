3cd58d0ee797489611784cc9ec8f1a0ed7747221
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-25 19:28:01 +0600

    feat(videoTitel): create new videoTitle component

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const MainContainer = () => {
+    const movies = useSelector((store) => store.movies?.nowPlayingMovies)
     return (
         <div>MainContainer</div>
     )
