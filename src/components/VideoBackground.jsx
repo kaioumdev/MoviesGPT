ce35c061e4bf225e20e713536ba4fa83ab4db1f7
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-26 01:32:18 +0600

    feat(movieId): implementing & using the movieId dynamically

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,24 +1,10 @@
-import React, { useEffect } from 'react'
-import { API_OPTIONS } from '../utils/constants'
-import { useDispatch, useSelector } from 'react-redux'
-import { addTrailerVideo } from '../utils/redux/moviesSlice'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import useMovieTrailer from '../hooks/useMovieTrailer'
 
 const VideoBackground = ({ movieId }) => {
-    const dispatch = useDispatch();
     const trailerVideo = useSelector(store => store.movies?.trailerVideo)
-    const getMovieVideo = async () => {
-        const data = await fetch('https://api.themoviedb.org/3/movie/822119/videos?language=en-US', API_OPTIONS)
-        const json = await data.json()
-        console.log(json)
-        const filterData = json.results.filter((video) => video.type === 'Trailer')
-        const trailer = filterData.length ? filterData[0] : json.results[0]
-        console.log(trailer)
-        dispatch(addTrailerVideo(trailer))
-    }
-
-    useEffect(() => {
-        getMovieVideo()
-    }, [])
+    useMovieTrailer(movieId)
     return (
         <div>
             <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + trailerVideo?.key} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
--- src/hooks/useMovieTrailer.jsx
+++ src/hooks/useMovieTrailer.jsx
@@ -3,15 +3,13 @@ import { API_OPTIONS } from "../utils/constants";
 import { addTrailerVideo } from "../utils/redux/moviesSlice";
 import { useEffect } from "react";
 
-const useMovieTrailer = () => {
+const useMovieTrailer = (movieId) => {
     const dispatch = useDispatch();
     const getMovieVideo = async () => {
-        const data = await fetch('https://api.themoviedb.org/3/movie/822119/videos?language=en-US', API_OPTIONS)
+        const data = await fetch('https://api.themoviedb.org/3/movie/' + movieId + '/videos?language=en-US', API_OPTIONS)
         const json = await data.json()
-        console.log(json)
         const filterData = json.results.filter((video) => video.type === 'Trailer')
         const trailer = filterData.length ? filterData[0] : json.results[0]
-        console.log(trailer)
         dispatch(addTrailerVideo(trailer))
     }
 
