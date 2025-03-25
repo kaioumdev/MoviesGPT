a1fdb70ebce3b1527b523f2c38416d25007f543c
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-25 16:25:09 +0600

    feat(gotMovies): successfully got movies from tmdb api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,11 @@
 export const LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 
 export const USER_AVATAR = "https://static.vecteezy.com/system/resources/previews/019/879/186/original/user-icon-on-transparent-background-free-png.png"
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNDU5ODAyMzJiM2ZkOWQ5NTA2MzllZTU0NmY2M2E4MiIsIm5iZiI6MTc0Mjg5Njc2My42NTY5OTk4LCJzdWIiOiI2N2UyN2U3YjQ0MGYzMTFhY2U3NWUzZjMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.zJb9rv_TLv-8k6HXwBMfgXeAKqYxf6_Es1EAbdUtV8g'
+    }
+};
