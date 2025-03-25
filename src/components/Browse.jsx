835792d7dda47199b80d4ac3287fe470761ee17d
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-25 16:37:00 +0600

    feat(moviesSlice): create new moviesSlice with addNowPlayingMovies

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -6,7 +6,7 @@ const Browse = () => {
     const getNowPlayingMovies = async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS)
         const json = await data.json()
-        console.log(json)
+        console.log(json.results)
     }
 
     useEffect(() => {
