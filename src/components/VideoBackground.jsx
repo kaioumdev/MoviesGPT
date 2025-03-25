b52733920781fdc7a1bc1e2bcbe82d04de78abc0
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-26 01:25:44 +0600

    feat(customHooks): create useMovieTrailer customHooks to manage movieTrailer

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -21,7 +21,7 @@ const VideoBackground = ({ movieId }) => {
     }, [])
     return (
         <div>
-            <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + trailerVideo.key} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
+            <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + trailerVideo?.key} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
         </div>
     )
 }
