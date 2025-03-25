8f4640a62052a3b5f0800feb05e4decb1302c301
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-26 03:58:27 +0600

    successfully fetch popular movies & make reducer for popular movies

--- src/hooks/usePopularMovies.jsx
+++ src/hooks/usePopularMovies.jsx
@@ -1,13 +1,14 @@
 import { useDispatch } from "react-redux"
 import { API_OPTIONS } from "../utils/constants"
 import { useEffect } from "react"
+import { addPopularMovies } from "../utils/redux/moviesSlice"
 
 const usePopularMovies = () => {
     const dispatch = useDispatch()
     const getPopularMovies = async () => {
-        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS)
+        const data = await fetch('https://api.themoviedb.org/3/movie/popular', API_OPTIONS)
         const json = await data.json()
-        dispatch(addNowPlayingMovies(json.results))
+        dispatch(addPopularMovies(json.results))
     }
 
     useEffect(() => {
