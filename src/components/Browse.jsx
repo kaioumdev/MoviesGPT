d43791e5f608cdf30e1b9edbd762662ca144c5de
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-25 20:22:15 +0600

    got mainMovie successfully

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './Header'
 import { API_OPTIONS } from '../utils/constants'
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
+import MainContainer from './MainContainer'
 
 const Browse = () => {
     useNowPlayingMovies()
     return (
         <div>
             <Header></Header>
+            <MainContainer></MainContainer>
         </div>
     )
 }
