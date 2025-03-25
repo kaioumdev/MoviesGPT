00b54025c3a9ff9d9839ba33a99e0102b0df560c
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-25 13:12:49 +0600

    feat(pthotoURL): successfully show the photoURL

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,11 @@ import { signOut } from 'firebase/auth';
 import React from 'react'
 import { auth } from '../utils/firebase';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
     const navigate = useNavigate();
+    const user = useSelector((store) => store.user)
     const handleSignOut = () => {
         signOut(auth).then(() => {
             navigate("/")
@@ -18,10 +20,12 @@ const Header = () => {
     return (
         <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between items-center'>
             <img className='w-44' src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="" />
-            <div className='flex p-2'>
-                <img className='h-12 w-12' src="https://static.vecteezy.com/system/resources/previews/019/879/186/original/user-icon-on-transparent-background-free-png.png" alt="User" />
-                <button onClick={handleSignOut} className='font-bold text-white'>Sign Out</button>
-            </div>
+            {
+                user && <div className='flex p-2'>
+                    <img className='h-12 w-12' src={user?.photoURL} alt="User" />
+                    <button onClick={handleSignOut} className='font-bold text-white'>Sign Out</button>
+                </div>
+            }
         </div>
     )
 }
