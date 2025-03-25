009eb0c9bc451d139f464ddf32d78caae7bd3604
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-26 01:07:05 +0600

    feat(reducer): create new addTrailerVideo reducer method at moviesSlice

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -16,7 +16,7 @@ const VideoBackground = ({ movieId }) => {
     }, [])
     return (
         <div>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/5PSzFLV-EyQ?si=y8S-wnFRS0p9od_X" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/5PSzFLV-EyQ?si=y8S-wnFRS0p9od_X" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
         </div>
     )
 }
