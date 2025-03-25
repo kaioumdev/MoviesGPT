ca58366be97472b736f7d682487ea39dbe893d0e
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-25 21:34:42 +0600

    successfully filter the trailer data singleMove

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -6,6 +6,9 @@ const VideoBackground = ({ movieId }) => {
         const data = await fetch('https://api.themoviedb.org/3/movie/822119/videos?language=en-US', API_OPTIONS)
         const json = await data.json()
         console.log(json)
+        const filterData = json.results.filter((video) => video.type === 'Trailer')
+        const trailer = filterData[0]
+        console.log(trailer)
     }
 
     useEffect(() => {
