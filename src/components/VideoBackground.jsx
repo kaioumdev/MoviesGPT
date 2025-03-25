cfb6065aacd8b09725998fa46176a7bc3e114d52
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-26 01:10:57 +0600

    successfully dispatch the movies trailer

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react'
 import { API_OPTIONS } from '../utils/constants'
+import { useDispatch } from 'react-redux'
+import { addTrailerVideo } from '../utils/redux/moviesSlice'
 
 const VideoBackground = ({ movieId }) => {
+    const dispatch = useDispatch()
     const getMovieVideo = async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/822119/videos?language=en-US', API_OPTIONS)
         const json = await data.json()
@@ -9,6 +12,7 @@ const VideoBackground = ({ movieId }) => {
         const filterData = json.results.filter((video) => video.type === 'Trailer')
         const trailer = filterData.length ? filterData[0] : json.results[0]
         console.log(trailer)
+        dispatch(addTrailerVideo(trailer))
     }
 
     useEffect(() => {
