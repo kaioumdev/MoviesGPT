f332f8d72e541f209f4315af805e54548de3a988
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-25 12:30:59 +0600

    navigate after signIn user successfully at browser page

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -5,7 +5,6 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-console.log("apiKey", import.meta.env.VITE_messagingSenderId)
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_apiKey,
     authDomain: import.meta.env.VITE_authDomain,
