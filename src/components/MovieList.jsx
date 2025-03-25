5c940c36e117e7aa15887910f8707541b430a490
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-26 03:03:25 +0600

    feat(movieList): complete movieList component styles

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import MovieCard from './MovieCard'
 
 const MovieList = ({ title, movies }) => {
-    console.log(movies)
     return (
-        <div>
-            <div>
-                <h1>{title}</h1>
-                <div>
-                    <MovieCard posterPath={movies[0]?.poster_path}></MovieCard>
+        <div className='p-6'>
+            <h1 className='text-3xl py-6'>{title}</h1>
+            <div className='flex overflow-x-scroll'>
+                <div className='flex'>
+                    {movies?.map(movie => <MovieCard key={movie.id} posterPath={movie.poster_path}></MovieCard>
+                    )}
                 </div>
             </div>
         </div>
