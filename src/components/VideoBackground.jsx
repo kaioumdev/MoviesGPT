4fec84000d7511ce445e344ff2b119f45d7f9e3a
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-25 21:29:49 +0600

    feat(singleMove): got singleMove by id dynamicall successfully

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { API_OPTIONS } from '../utils/constants'
 
-const VideoBackground = () => {
+const VideoBackground = ({ movieId }) => {
+    const getMovieVideo = async () => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/822119/videos?language=en-US', API_OPTIONS)
+        const json = await data.json()
+        console.log(json)
+    }
+
+    useEffect(() => {
+        getMovieVideo()
+    }, [])
     return (
         <div>VideoBackground</div>
     )
