f6756bd666cd689a9ce9c5bcd6c19dc4e8bb6889
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-26 15:33:20 +0600

    implements background image to the gptSearch component

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -6,7 +6,7 @@ const GptSearchBar = () => {
             <form className='w-1/2 bg-black grid grid-cols-12'>
                 <input
                     type="text"
-                    className='p-4 m-4 col-span-9'
+                    className='p-4 m-4 col-span-9 bg-white'
                     placeholder='What would you like to watch today?'
                 />
                 <button className='col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg'>Search</button>
