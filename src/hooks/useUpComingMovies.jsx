06f46864b724bb0a46ce8dad5e11cc0482ae3314
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-26 12:45:07 +0600

    feat(hooks): create custorm hooks for upcoming movies

---
+++ src/hooks/useUpComingMovies.jsx
@@ -0,0 +1,19 @@
+import { useDispatch } from "react-redux"
+import { API_OPTIONS } from "../utils/constants"
+import { useEffect } from "react"
+import { addTopRatedMovies } from "../utils/redux/moviesSlice"
+
+const useUpComingMovies = () => {
+    const dispatch = useDispatch()
+    const getUpComingMovies = async () => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/upcoming', API_OPTIONS)
+        const json = await data.json()
+        dispatch(addTopRatedMovies(json.results))
+    }
+
+    useEffect(() => {
+        getUpComingMovies()
+    }, [])
+}
+
+export default useUpComingMovies
