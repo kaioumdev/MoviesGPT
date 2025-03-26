7ca0bcee868916d5b02cb66bd374e4b3dc23172a
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-26 12:48:16 +0600

    feat(dispatch): dispatch upComingMovies successfully by redux

--- src/hooks/useUpComingMovies.jsx
+++ src/hooks/useUpComingMovies.jsx
@@ -1,14 +1,14 @@
 import { useDispatch } from "react-redux"
 import { API_OPTIONS } from "../utils/constants"
 import { useEffect } from "react"
-import { addTopRatedMovies } from "../utils/redux/moviesSlice"
+import { addUpComingMovies } from "../utils/redux/moviesSlice"
 
 const useUpComingMovies = () => {
     const dispatch = useDispatch()
     const getUpComingMovies = async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/upcoming', API_OPTIONS)
         const json = await data.json()
-        dispatch(addTopRatedMovies(json.results))
+        dispatch(addUpComingMovies(json.results))
     }
 
     useEffect(() => {
