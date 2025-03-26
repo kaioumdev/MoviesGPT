ffb7a31042bd65fa4ca29115f3bb3dc319e76529
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-26 19:26:21 +0600

    feat(lang): create new languageConstants component successfully

---
+++ src/utils/languageConstants.js
@@ -0,0 +1,16 @@
+const lang = {
+    en: {
+        search: "Search",
+        gptSearchPlaceholder: "What would you like to watch today?"
+    },
+    bengali: {
+        search: "অনুসন্ধান করুন",
+        gptSearchPlaceholder: "আজ তুমি কী দেখতে চাও?"
+    },
+    spanish: {
+        search: "Buscar",
+        gptSearchPlaceholder: "¿Qué deseas ver hoy?"
+    }
+}
+
+export default lang;
