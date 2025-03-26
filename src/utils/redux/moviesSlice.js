068b3769f01a032abd3e7985637515b743eb3046
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-26 12:46:39 +0600

    feat(reducer): create new reducer for upComingMovies at moviesSlice

--- src/utils/redux/moviesSlice.js
+++ src/utils/redux/moviesSlice.js
@@ -6,6 +6,7 @@ const moviesSlice = createSlice({
         nowPlayingMovies: null,
         popularMovies: null,
         topRatedMovies: null,
+        upComingMovies: null,
         trailerVideo: null,
     },
     reducers: {
@@ -18,12 +19,15 @@ const moviesSlice = createSlice({
         addTopRatedMovies: (state, action) => {
             state.topRatedMovies = action.payload
         },
+        addUpComingMovies: (state, action) => {
+            state.upComingMovies = action.payload
+        },
         addTrailerVideo: (state, action) => {
             state.trailerVideo = action.payload
         }
     }
 })
 
-export const { addNowPlayingMovies, addTrailerVideo, addPopularMovies, addTopRatedMovies } = moviesSlice.actions
+export const { addNowPlayingMovies, addTrailerVideo, addPopularMovies, addTopRatedMovies, addUpComingMovies } = moviesSlice.actions
 
 export default moviesSlice.reducer;
