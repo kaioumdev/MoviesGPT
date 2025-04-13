63237cfe3f7c346037dc0d16f26617805d565668
kaiyumdev <afrankaiyum34@gmail.com>
2025-04-13 19:55:12 +0600

    fix: remove OpenAI key from code and use .env instead

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -1,19 +1,27 @@
 import React, { useRef } from 'react'
 import lang from '../utils/languageConstants'
 import { useSelector } from 'react-redux'
-import { openai } from '../utils/openai';
+import openai from '../utils/openai';
 
 const GptSearchBar = () => {
     const langKey = useSelector(store => store.config.lang);
     const searchText = useRef(null)
     const handleGptSearchClick = async () => {
         console.log(searchText.current.value)
-        // const gptQuery = "Act as a Movie Recommendation system and suggest some movies for the query: " + searchText.current.value + "Only give me names or 5 movies, comma separated like the example results given ahead. Examples result 3idiots, 12Fail, Dada, Hi Nanna, PK"
-        const result = await openai.chat.completions.create({
-            model: 'gpt-4o',
-            messages: [{ role: 'user', content: searchText.current.value }],
+        const gptQuery = "Act as a Movie Recommendation system and suggest some movies for the query: " + searchText.current.value + "Only give me names or 5 movies, comma separated like the example results given ahead. Examples result: 3idiots, 12Fail, Dada, Hi Nanna, PK"
+        // const result = await openai.chat.completions.create({
+        //     model: 'gpt-4o',
+        //     messages: [{ role: 'user', content: searchText.current.value }],
+        // });
+        // console.log(result.choices);
+
+        const gptResults = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                { role: 'user', content: gptQuery },
+            ],
         });
-        console.log(result.choices);
+        console.log(gptResults.choices)
     }
     return (
         <div className='pt-[10%] flex justify-center'>
