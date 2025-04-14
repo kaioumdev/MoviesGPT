08e6500a6d747edb5a7aad265c4c4117894c40e9
kaiyumdev <afrankaiyum34@gmail.com>
2025-04-14 21:47:43 +0600

    feat(memorization): implements memorization at the upComingMovies component

--- src/hooks/useUpComingMovies.jsx
+++ src/hooks/useUpComingMovies.jsx
@@ -1,10 +1,11 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { API_OPTIONS } from "../utils/constants"
 import { useEffect } from "react"
 import { addUpComingMovies } from "../utils/redux/moviesSlice"
 
 const useUpComingMovies = () => {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const upComingMovies = useSelector(store => store.movies.upComingMovies)
     const getUpComingMovies = async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/upcoming', API_OPTIONS)
         const json = await data.json()
@@ -12,7 +13,7 @@ const useUpComingMovies = () => {
     }
 
     useEffect(() => {
-        getUpComingMovies()
+        !upComingMovies && getUpComingMovies()
     }, [])
 }
 
