bb5e16750f3401606223621fd86a6d91a4f33571
kaiyumdev <afrankaiyum34@gmail.com>
2025-04-14 21:44:27 +0600

    feat(memorization): implements memorization at the trailerMovies component

--- src/hooks/useMovieTrailer.jsx
+++ src/hooks/useMovieTrailer.jsx
@@ -1,10 +1,11 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addTrailerVideo } from "../utils/redux/moviesSlice";
 import { useEffect } from "react";
 
 const useMovieTrailer = (movieId) => {
     const dispatch = useDispatch();
+    const trailerVideo = useSelector(store => store.movies.trailerVideo)
     const getMovieVideo = async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/' + movieId + '/videos?language=en-US', API_OPTIONS)
         const json = await data.json()
@@ -14,7 +15,7 @@ const useMovieTrailer = (movieId) => {
     }
 
     useEffect(() => {
-        getMovieVideo()
+        !trailerVideo && getMovieVideo()
     }, [])
 }
 
