eca67f69cfd2a79afdb040d2fb345ac34591c432
kaiyumdev <afrankaiyum34@gmail.com>
2025-04-14 21:45:34 +0600

    feat(memorization): implements memorization at the popularMovies component

--- src/hooks/usePopularMovies.jsx
+++ src/hooks/usePopularMovies.jsx
@@ -1,10 +1,11 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { API_OPTIONS } from "../utils/constants"
 import { useEffect } from "react"
 import { addPopularMovies } from "../utils/redux/moviesSlice"
 
 const usePopularMovies = () => {
     const dispatch = useDispatch()
+    const popularMovies = useSelector(store => store.movies.popularMovies)
     const getPopularMovies = async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/popular', API_OPTIONS)
         const json = await data.json()
@@ -12,7 +13,7 @@ const usePopularMovies = () => {
     }
 
     useEffect(() => {
-        getPopularMovies()
+        !popularMovies && getPopularMovies()
     }, [])
 }
 
