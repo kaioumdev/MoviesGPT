9697c2d5fb2fdb14f2a513a52f09be987beb63a2
kaiyumdev <afrankaiyum34@gmail.com>
2025-04-14 21:42:57 +0600

    feat(memorization): implements memorization at the nowPlayingMovies component

--- src/hooks/useNowPlayingMovies.jsx
+++ src/hooks/useNowPlayingMovies.jsx
@@ -1,10 +1,11 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { addNowPlayingMovies } from "../utils/redux/moviesSlice"
 import { useEffect } from "react"
 import { API_OPTIONS } from "../utils/constants"
 
 const useNowPlayingMovies = () => {
     const dispatch = useDispatch()
+    const nowPlayingMovies = useSelector(store => store.movies.nowPlayingMovies)
     const getNowPlayingMovies = async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS)
         const json = await data.json()
@@ -12,7 +13,7 @@ const useNowPlayingMovies = () => {
     }
 
     useEffect(() => {
-        getNowPlayingMovies()
+        !nowPlayingMovies && getNowPlayingMovies()
     }, [])
 }
 
