8b87d68b46249447a4974f6aa6fc6914f605743c
kaiyumdev <afrankaiyum34@gmail.com>
2025-04-14 21:17:41 +0600

    feat(coplete): complete component of gptMovieSuggestion to successfully show the suggested movies

--- src/components/GptMovieSuggestion.jsx
+++ src/components/GptMovieSuggestion.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import MovieList from './MovieList';
 
 const GptMovieSuggestion = () => {
+    const { movieResults, movieNames } = useSelector(store => store.gpt)
+    if (!movieNames) return null
     return (
-        <div>GptMovieSuggestion</div>
+        <div>
+            <div>
+                {
+                    movieNames.map((movieName, index) => (<MovieList key={movieName} title={movieName} movies={movieResults[index]}></MovieList>))
+                }
+            </div>
+        </div>
     )
 }
 
